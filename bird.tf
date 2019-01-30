e29f612f78006e5a6e671db340ff06a477cd887e
John Studarus <john@jhlconsulting.com>
2019-01-30 19:42:19 +0000

    split code by function to better support multiple hosts

---
+++ bird.tf
@@ -0,0 +1,30 @@
+
+resource "packet_reserved_ip_block" "elastic_ip" {
+  project_id = "${var.packet_project_id}"
+  facility   = "${var.packet_facility}"
+  quantity   = 1
+}
+
+data "template_file" "configure_network" {
+  template = "${file("templates/configure_network.tpl")}"
+
+  vars = {
+    elastic_ip = "${cidrhost(packet_reserved_ip_block.elastic_ip.cidr_notation,0)}"
+  }
+}
+
+data "template_file" "bird_conf" {
+  template = "${file("templates/bird.tpl")}"
+
+  vars = {
+    MD5 = "${var.bgp_md5}"
+  }
+}
+
+data "template_file" "configure_bird" {
+  template = "${file("templates/configure_bird.tpl")}"
+
+  vars = {
+    token = "${var.packet_auth_token}"
+  }
+}
