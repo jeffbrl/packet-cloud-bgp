336a1b13b3ccb1db05d4af049cd6c081ad593f37
John Studarus <john@jhlconsulting.com>
2019-02-06 19:51:48 +0000

    display elastic IPs that go along with each facility

--- output.tf
+++ output.tf
@@ -6,6 +6,15 @@ output "Server IP @ Facility 2" {
   value = "${packet_device.host-2.access_public_ipv4}"
 }
 
+output "Elastic IP @ Facility 1" {
+  value = "${packet_reserved_ip_block.elastic_ip-1.cidr_notation}"
+}
+
+output "Elastic IP @ Facility 2" {
+  value = "${packet_reserved_ip_block.elastic_ip-2.cidr_notation}"
+}
+
+
 output "Anycast IP" {
   value = "TODO"
 }
