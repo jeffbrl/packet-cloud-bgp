9dedd3ab495407e352f5ce048ce6ee96501802ed
John Studarus <john@jhlconsulting.com>
2019-02-06 20:00:57 +0000

    replaced with elastic IP output

--- output.tf
+++ output.tf
@@ -1,5 +1,5 @@
 output "Server IP @ Facility 1" {
-  value = "${packet_device.host-1.access_public_ipv4}"
+  value = "${packet_device.host-1.*.access_public_ipv4}"
 }
 
 output "Server IP @ Facility 2" {
@@ -12,9 +12,4 @@ output "Elastic IP @ Facility 1" {
 
 output "Elastic IP @ Facility 2" {
   value = "${packet_reserved_ip_block.elastic_ip-2.cidr_notation}"
-}
-
-
-output "Anycast IP" {
-  value = "TODO"
 }
