7c70a57bdb4fe00af2405f453af7c9a11f619610
Jeff Loughridge <jeffl@konekti.us>
2019-02-22 15:08:19 +0000

    Set hostname based on site

--- instance.tf
+++ instance.tf
@@ -12,8 +12,7 @@ resource "packet_reserved_ip_block" "elastic_ip" {
 resource "packet_device" "hosts" {
   depends_on = ["packet_ssh_key.ssh-key"]
 
-  #  hostname = "${format("%s-%02d", ${var.packet_facilities_global_ipv4[count.index]}, 1)}"
-  hostname         = "test"
+  hostname = "${format("%s-%02d", "${var.sites[count.index]}", 1)}"
   plan             = "t1.small.x86"
   facilities       = [ "${var.sites[count.index]}" ]
   operating_system = "ubuntu_18_04"
